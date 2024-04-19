7dfd0a99c52cace04e1c238625a97fd3f4483139
myferonguilded <michaelthecrumbyt1@gmail.com>
2024-04-19 12:03:02 -0500

    Update App.jsx, adding ReGuilded button.

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -41,6 +41,12 @@ function App() {
               <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>
             </button>
           </a>
+          <a href="https://www.guilded.gg/ReGuilded/groups/RdK6o7jD/channels/d9b8d0c4-c213-4e65-b82b-c4a81facba79/forums/1088706986" target='_blank'>
+            <button className='reguilded-button'>
+              <span className='reguildedText'>ReGuilded</span>
+              <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>
+            </button>
+          </a>
         </div>
       </div>
     </>
